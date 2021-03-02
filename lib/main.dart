2e2f508b6711cb0741fcc928b84d4721183658ea
Denzell Grant <denzell.grant@embryhealth.com>
2021-03-01 19:04:36 -0700

    pushing project

--- lib/main.dart
+++ lib/main.dart
@@ -1,108 +1,106 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
-  runApp(App());
-}
-
-class App extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: HomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class HomePage extends StatefulWidget {
-  HomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the HomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Color.fromRGBO(0, 153, 0, 0.4),
+        appBar: AppBar(
+          title: Text('Dice'),
+          backgroundColor: Color.fromRGBO(0, 153, 0, 0.4),
         ),
+        body: DicePage(),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    ),
+  );
+}
+
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+//Static language but 'dynamic' keyword makes it dynamic
+
+class _DicePageState extends State<DicePage> {
+  //State of stateless widgets are not meant to change
+  //State is the way how things are or displayed
+  //Updates or changes? Stateful Widget.
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+  int diceTotal = 0;
+
+  @override
+  //Build method refreshes every time the page is saved.
+  Widget build(BuildContext context) {
+    Random random = new Random();
+    void randomDice() {
+      setState(() {
+        leftDiceNumber = random.nextInt(6) + 1;
+        rightDiceNumber = random.nextInt(6) + 1;
+        diceTotal = leftDiceNumber + rightDiceNumber;
+      });
+    }
+
+    // void diceAmount() {
+    //   setState(() {
+    //     var diceTotal = leftDiceNumber + rightDiceNumber;
+    //     // print(diceTotal);
+    //   });
+    // }
+
+    print(diceTotal);
+
+    return Center(
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 130.0),
+                child: Text(
+                  "$diceTotal",
+                  textAlign: TextAlign.center,
+                  overflow: TextOverflow.ellipsis,
+                  style: TextStyle(
+                    fontWeight: FontWeight.w900,
+                    fontSize: 100,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 200,
+              ),
+            ],
+          ),
+          Row(
+            children: <Widget>[
+              Expanded(
+                //Expanded uses flex like Bootstrap
+                //Expanded widget automatically fills up the screen space
+                child: FlatButton(
+                  child: Image.asset('images/dice$leftDiceNumber.png'),
+                  onPressed: () {
+                    randomDice();
+                    // diceAmount();
+                  },
+                ),
+              ),
+              Expanded(
+                child: FlatButton(
+                  child: Image.asset('images/dice$rightDiceNumber.png'),
+                  onPressed: () {
+                    randomDice();
+                    // diceAmount();
+                  },
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
